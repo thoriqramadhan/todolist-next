3694b819e5246744aabd8caed84c0586b40165fd
thoriqramadhan <respect.thoriq@gmail.com>
2024-12-06 16:43:36 +0700

    refactor: adding tags to todo card

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -3,7 +3,7 @@ import { Check, Trash, Undo2 } from 'lucide-react';
 import Timer from './Timer';
 import { Dispatch, FC, SetStateAction, useEffect, useState } from 'react';
 import { cn, formatDate, formatToISO } from '@/lib/utils';
-import { updateTodo } from '@/helpers/dbHelpers';
+import { getTag, updateTodo } from '@/helpers/dbHelpers';
 interface TodoCardProps {
     todo: {
         id?: number,
@@ -12,12 +12,14 @@ interface TodoCardProps {
         start: Date,
         deadline: Date,
         finishedAt: Date | null,
+        todoTagId: number
     },
     setter: Dispatch<SetStateAction<never[]>>
 }
 export const TodoCard: FC<TodoCardProps> = ({ todo, setter }) => {
     const [TodoData, setTodoData] = useState(todo)
     const { id, status, title, start, deadline, finishedAt } = TodoData;
+    const [tag, setTag] = useState('')
     const deadlineConfig = status == 'failed' ? 0 : deadline;
     const [isDeadline, setIsDeadline] = useState(false)
     async function deleteHandler() {
@@ -99,6 +101,21 @@ export const TodoCard: FC<TodoCardProps> = ({ todo, setter }) => {
         }
     }
     useEffect(() => {
+
+        const tagInit = async () => {
+            console.log('fetching ');
+            const response = await fetch(`/api/tags/${todo.todoTagId}`)
+            if (!response.ok) {
+                return;
+            }
+            const parsedResponse = await response.json()
+            setTag(parsedResponse.name)
+
+        }
+        tagInit()
+
+    }, [])
+    useEffect(() => {
         try {
             if (isDeadline) {
                 failedHandler(id)
@@ -111,6 +128,7 @@ export const TodoCard: FC<TodoCardProps> = ({ todo, setter }) => {
         <div className="w-full h-28 bg-gra-50 shadow-md p-3 rounded-lg border relative bg-white">
             <h1 className='font-medium tracking-wider mb-1'>{title}  -   <span className={cn('text-xs tracking-widest text-white p-1 rounded-full', { 'bg-green-400': status == 'finished', 'bg-yellow-400': status == 'ongoing', 'bg-red-500': status == 'failed' })}>{status}</span></h1>
             <Timer startDate={start} deadline={deadlineConfig} finishedAt={finishedAt} setter={[setIsDeadline, setTodoData]} />
+            <p>{tag}</p>
             <div className="flex absolute top-5 right-3 gap-x-3">
                 {isDeadline || status == 'failed' ? '' : status == 'finished' ? <Undo2 size={20} className='cursor-pointer' onClick={redoHandler} /> : <Check size={20} className='cursor-pointer' onClick={finishHandler} />}
                 <Trash size={20} className='cursor-pointer' onClick={deleteHandler} />
