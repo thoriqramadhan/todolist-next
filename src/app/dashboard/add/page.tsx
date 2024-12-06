d3f68fc1c8ade5b3721fc0a715f3c403820f666e
thoriqramadhan <respect.thoriq@gmail.com>
2024-12-06 16:42:36 +0700

    feat: add validation on tag array empty

--- src/app/dashboard/add/page.tsx
+++ src/app/dashboard/add/page.tsx
@@ -1,14 +1,25 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { SelectTrigger, Select, SelectContent, SelectItem } from '@/components/ui/select';
+import { getTags } from '@/helpers/dbHelpers';
 import { createTodo } from '@/lib/action/todo-crud';
 import { Label } from '@radix-ui/react-label';
+import { SelectValue } from '@radix-ui/react-select';
 import { FC } from 'react';
+import SelectTags from './SelectTags';
 
 interface PageProps {
 
 }
 
-const Page: FC<PageProps> = ({ }) => {
+const Page: FC<PageProps> = async ({ }) => {
+    const tags = await getTags();
+    if (tags.length == 0) {
+
+        return (
+            <p>Please add a tag first blud!</p>
+        )
+    }
     return <form action={createTodo} className='grid gap-5 w-full grid-cols-4'>
         <h1 className='text-title col-span-4'>Create Todo</h1>
         <section className='col-span-2 flex flex-col'>
@@ -22,6 +33,9 @@ const Page: FC<PageProps> = ({ }) => {
         <section className='col-span-2 flex flex-col'>
             <Label htmlFor="deadline" className='text-lg font-medium block'>Deadline</Label>
             <Input type='datetime-local' name='deadline' id='deadline' className='w-full' autoComplete='off' required />
+        </section>
+        <section className='col-span-2 flex flex-col'>
+            <SelectTags tagsArray={tags} />
         </section>
         <Button className=' col-span-4 space-x-4 disabled:cursor-not-allowed'>Create</Button>
         {/* {!!formState?.error?.email && <ErrorInput>{formState?.error?.email ?? ''}</ErrorInput>} */}
